098180bc64643f7bc9e460442519f59107ffcba4
Mandy Jackson <mandy@high10digital.com>
2021-07-27 20:43:50 -0400

    More visual changes and updates to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,25 @@
-// this funciton immediately below is actually jQuery, jQuery makes working with the DOM a little bit easier than plain jane js but you are not bringing it in to your html page so javascript doesnt know how to read it
-// google jQuery cdn if you wanted to use it in this project.
-
 function() {
 	('.toggler').on('click', function() {
 		('nav').slideToggle(500);
   });
 };
 
-function submitFirstName(event)
-{
-    console.log(event)
-	event.preventDefault();
-	// on submit, test the name field//
-	 let firstName = document.getElementById ('name');
-	 
-	 if (firstName.value) {
-		 alert('Success! Your info was submitted.');
-	 } else {
-		 alert('invalud first name. Please try again')
-	 }
-}
-
+      // Contact Us Form
+      function clickMe() {
+        let myForm = document.getElementById('myForm');
+        let formData = new FormData(myForm);
+  
+        let firstName = (formData.get('first_Name'));
+        let lastName = (formData.get('last_Name'));
+  
+        if (lastName.length > 3) {
+          alert(firstName + "is a valid name!")
+      } else {
+          alert(firstName + "is not long enough")
+        }
+        console.log(firstName, lastName);
+    
+    }
 
 class Navbar {
     constructor(target, menu) {
